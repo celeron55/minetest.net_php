4be10f7c326271e24111ce35dea808f5ec638032
BlockMen <blockmen@trash-mail.com>
2013-03-19 14:15:05 +0100

    Template is done so far
    Beside the changes for the subpages there are just a little changes to
    be done

--- index.php
+++ index.php
@@ -1,116 +1,81 @@
 <?php
-$title = "About";
-include("header.php");
-?>
+$title = "Minetest";
+$notify_type = "info";
+$msg = "";
 
-<h2>Minetest</h2>
 
-<div style="float:right;">
+$top = "
 
-<a href="images/screenshot_terrain_2012-12-03.png">
-<img style="margin: 0em 0em 3em 3em; width: 400px;" alt="screenshot" src="images/screenshot_terrain_2012-12-03_small.jpg"/>
-</a>
-<br />
-
-<a href="images/screenshot_terrain_2012-06-03.png">
-<img style="margin: 0em 0em 3em 3em; width: 400px;" alt="screenshot" src="images/screenshot_terrain_2012-06-03_small.jpg"/>
-</a>
-<br />
-
-<!--<a href="images/cisountexturetrees.png">
-<img style="margin: 0em 0em 3em 3em; width: 400px;" alt="screenshot" src="images/cisountexturetrees_small.jpg"/>
-</a>
-<br />-->
-
-<a href="images/screenshot_cave_2012-06-03.png">
-<img style="margin: 0em 0em 3em 3em; width: 400px;" alt="screenshot" src="images/screenshot_cave_2012-06-03_small.jpg"/>
-</a>
-<br />
+<div id='menu'>
+<ul>
+<li><a href='#'>Home</a></li>
+<li><a href='#'>Download</a></li>
+<li><a href='#'>Mods</a></li>
+<li><a href='#'>Texture Packs</a></li>
+<li><a href='#'>Community</a>
+	<ul>
+	<li><a href='#' class='sub'>Forum</a></li>
+	<li><a href='#' class='sub'>Contribute</a></li>
+	</ul>
+</li>
+<li><a href='#'>Wiki</a></li>
+<li><a href='#'>Development</a>
+	<ul>
+	<li><a href='#' class='sub'>Github</a></li>
+	<li><a href='#' class='sub'>Api</a></li>
+	<li><a href='#' class='sub'>Blog</a></li>
+	</ul>
+</li>
+<li style='float:right;'><a href='#'>Support us</a></li>
+</ul>
 
 </div>
 
-<!--
-<p>
-Minetest-c55 is an InfiniMiner/Minecraft inspired game intended
-for me to show my friends how fucking superior I am at programming
-compared to them!
-</p>
-<p>
-Uhm... No... Well, whatever.
-</p>
--->
-<p>
-Minetest is an<!--a free and open source--> infinite-world block sandbox game and a game engine, inspired by InfiniMiner, Minecraft and the like. It has been in development and use since October 2010.
-</p>
-<p>
-It is Free and Open Source Software, released under the LGPL, available for Windows and Linux.<!-- This way it is more readily available to Linux users, and random people (you!) can fix bugs and experiment effectively.-->
-</p>
-<p>
-Minetest is developed by <a href="https://www.ohloh.net/p/minetest-c55/contributors?sort=latest_commit">a random group of lunatics</a>.
-</p>
-<!--<p>
-If you like the game or would like to see more games from me, you should consider <a href="donations.php">donating</a>.
-</p>-->
-<p>
-Minetest is technically simple, stable and portable. It is lightweight enough to run on fairly old hardware. It currently runs playably on a laptop with Intel 945GM graphics. Though, as for the CPU, dualcore is recommended.
-</p>
-<!--<p>
-<b>This game is under development</b>, and as of now, the game does not really differ from Minecraft except for having a lot less features. Still, playing is quite fun already, especially for people who have not been able to experience Minecraft.
-</p>-->
 
-<!--<h2 style="clear: both;">Features</h2>-->
-<h2>Features</h2>
-<ul id="features">
-<li>
-Walk around, dig and build in an infinite voxel world (or boxel, as reddit calls it), and craft stuff from raw materials to help you along the way. We hope to add in some survival elements, but not much really exist ATM.
-</li>
-<li>
-Sinfully easy server-side
-<a href="http://minetest.net/forum/viewforum.php?id=11">modding</a>
-<a href="http://api.minetest.net/">API</a>.
-<a href="https://github.com/minetest/minetest/blob/stable-0.4/doc/lua_api.txt">(short)</a>
-</li>
-<li>
-Multiplayer support for tens of players, via servers hosted by users<br/>
-</li>
-<li>
-Voxel based dynamic lighting (quite similar to Minecraft; light up caves with torches)<br/>
-</li>
-<li>
-Almost infinite world and a fairly good map generator (limited to +-31000 blocks in all directions at the moment)<br />
-</li>
-<li>
-Runs natively on Windows and Linux (C++ and Irrlicht. No Java.)
-</li>
-</ul>
+<div style='position:absolute; top:41px; z-index:99; padding-left:8px;'><img src='./graph/minetest.png' class='no' style='z-index:99;' /></div>";
 
-<h2>Links &amp; other</h2>
-<p>
-<ul>
-<!--<li>
-<a href="http://">http://</a>
-</li>-->
-<li>
-IRC Channels:
-<ul>
-<li>#minetest @ Freenode</li>
-<li>#minetest-dev @ Freenode</li>
-</ul>
-</li>
-<li>
-Authors:
-<ul>
-<li>
-Core developers: umm... how about <a href="https://www.ohloh.net/p/minetest-c55/contributors?sort=latest_commit">ohloh</a>?
-</li>
-<li>
-Original author: Perttu Ahola, celeron55 @ IRCnet and gmail.com
-</li>
-</ul>
-</li>
-</ul>
+$main = "
+<!--<h2>Caption</h2>-->
+<div style='float:left; width:576px;'> <!--the left part-->
+	<span class='page'>
+		<img src='./images/screen1.png' class='no' style='box-shadow: #000000 0px 0px 3px, #000000 1px 1px 2px; margin-bottom:32px;' />
+		<span>Minetest</span> is an infinite-world block sandbox game and a game engine, inspired by InfiniMiner, Minecraft and the like. It has been in development and use since October 2010.
+		<br /><br />
+		It is Free and Open Source Software, released under the LGPL, available for Windows and Linux.<!-- This way it is more readily available to Linux users, and random people (you!) can fix bugs and experiment effectively.-->
+		<br /><br />
+		Minetest is developed by a random group of lunatics</a>.
+		<br /><br />
+		Minetest is technically simple, stable and portable. It is lightweight enough to run on fairly old hardware. It currently runs playably on a laptop with Intel 945GM graphics. Though, as for the CPU, dualcore is recommended.
+        </span>
 
-<?php
-include("footer.php");
+</div>
+
+<div style='float:left; width:305px;'> <!--the right box-->
+	<img src='./graph/minetest-icon-120.png' class='no' style='width:80px; float:left;' />
+	<div style='float:left; padding-left:10px;'>
+		<h3><b>Download</b></h3>
+		<a href=''>0.4.5 for Windows</a><br />
+		<a href=''>0.4.5 for Linux</a><br />
+		<a href='' class='sub'>for other Distributions</a>
+
+
+		<h3 style='padding-top:32px;'>Mods</h3>
+		<a href=''>Most popular</a><br />
+		<a href=''>Newest</a><br />
+		<a href='' class='sub'>browse all Mods</a>
+
+
+		<h3 style='padding-top:32px;'>Texture Packs</h3>
+		<a href=''>Most popular</a><br />
+		<a href=''>Newest</a><br />
+		<a href='' class='sub'>browse all Texture Packs</a>
+	</div>
+</div>
+
+";
+
+$foot = "#footer";
+
+
+include("./lay/dummy.php")
 ?>
-
--- lay/top_start.php
+++ lay/top_start.php
@@ -2,6 +2,7 @@
 echo "
 <body>
 <div id='container' align='center'>
+<div style='width:100%; height:32px; position:fixed; top:0; z-index:100; background-image:url(./graph/nav_bar.png);'>&nbsp;</div>
 	<div id='box'>
 
 		<div id='bxm_con'>
